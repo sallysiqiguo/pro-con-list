8929a280cb55fbae3ec2f1f7c4a724b6283a906c
Sally Guo <siqig@andrew.cmu.edu>
2018-11-05 23:15:25 -0500

    Add typed.js and use local storage

--- src/App.js
+++ src/App.js
@@ -5,29 +5,72 @@ import './App.css';
 import Column from './Components/Column'
 import NewItem from './Components/NewItem'
 
+import Typed from 'typed.js'
+
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      title: "tryna make a decision",
-      pros: [],
-      cons: [] 
+      title: localStorage.getItem('title'),
+      pros: localStorage.getItem('pros') === null ? [] : JSON.parse(localStorage.getItem('pros')),
+      cons: localStorage.getItem('cons') === null ? [] : JSON.parse(localStorage.getItem('cons')) 
     };
+    this.handleTitleChange = this.handleTitleChange.bind(this)
+    this.clearLocalStorage = this.clearLocalStorage.bind(this)
+  }
+
+  handleTitleChange(e) {
+    localStorage.setItem('title', e.target.value);
+    this.setState({
+      title: localStorage.getItem('title')
+    })
   }
 
   handleSubmit = (e, isPro) => {
     e.preventDefault()
+    localStorage.setItem('pros', JSON.stringify(isPro ? [...this.state.pros, e.target[0].value] : this.state.pros));
+    localStorage.setItem('cons', JSON.stringify(!isPro ? [...this.state.cons, e.target[0].value] : this.state.cons));
     this.setState({
-      pros: isPro ? [...this.state.pros, e.target[0].value] : this.state.pros,
-      cons: !isPro ? [...this.state.cons, e.target[0].value] : this.state.cons
+      pros: JSON.parse(localStorage.getItem('pros')),
+      cons: JSON.parse(localStorage.getItem('cons'))
     });
     e.target[0].value = ""
+  }
+
+  clearLocalStorage() {
+    localStorage.clear()
+    this.setState({
+      title: "",
+      pros: [],
+      cons: []
+    })
+  }
+
+  componentDidMount() {
+    const options = {
+      strings: [
+        'I wanna decide on...', 
+        '"Should I go out with my friends tonight?"', 
+        '"Should I work from home tomorrow?"', 
+        '"Should I go to bed now?"'
+      ],
+      typeSpeed: 30,
+      backSpeed: 30,
+      attr: 'placeholder',
+      bindInputFocusEvents: true,
+      loop: true
+    };
+    // this.el refers to the <span> in the render() method
+    this.typed = new Typed(this.el, options);
   }
 
   render() {
     return (
       <div>
-        <h1>{this.state.title}</h1>
+        <h1 className="pure-g">
+          <input className='title pure-u-1' ref={(el) => { this.el = el; }} onChange={this.handleTitleChange} value={this.state.title === null ? "" : this.state.title}></input>
+        </h1>
         <div className="pure-g">
           <Column isPro={true} data={this.state.pros}/>
           <Column isPro={false} data={this.state.cons}/>
@@ -36,7 +79,7 @@ class App extends Component {
           <NewItem isPro={true} handleSubmit={this.handleSubmit}/>
           <NewItem isPro={false} handleSubmit={this.handleSubmit}/>
         </div>
-        
+        <button type="button" onClick={this.clearLocalStorage}> clear dis bih </button>
       </div>
     );
   }
